d4e30f5242628cb0a46b4ad0a430d4a56f84f68a
Phatchanon Chuchat <pchuchat@student.jyu.fi>
2023-02-12 16:36:40 +0200

    kaksi pelaa controloiminen

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+    private Vector2 movementInput;
+
+
+ 
+
+
+    private void Update()
+    {
+        transform.Translate(new Vector3(movementInput.x, 0, movementInput.y) * speed * Time.deltaTime);
+    }
+
+ 
+
+      
+    
+    public void OnMove(InputAction.CallbackContext ctx) =>movementInput = ctx.ReadValue<Vector2>();
+    
+
+  
+}
