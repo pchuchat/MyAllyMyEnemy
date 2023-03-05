2dfc7d9c2b69c53a6db26432caabd67ea2c0bb45
Phatchanon Chuchat <pchuchat@student.jyu.fi>
2023-03-05 11:17:35 +0200

    komenttit lisätty scriptiin

--- Assets/Script/CharcterSwitcher.cs
+++ Assets/Script/CharcterSwitcher.cs
@@ -2,22 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+// ©GameGurus - Heikkinen R., Hopeasaari J., Kantola J., Kettunen J., Kommio R, PC, Parviainen P., Rautiainen J.
+// Creator: PC, Phatchanon Chuchat
 
-public class CharcterSwitcher: MonoBehaviour
+// spaw two different fighter
+
+public class CharcterSwitcher : MonoBehaviour
 {
+    // Index of the currently selected fighter in the list
     int index = 0;
+
+    // List of fighter GameObjects to be spawned
     [SerializeField] List<GameObject> fighters = new List<GameObject>();
+
+    // Reference to the PlayerInputManager component
     PlayerInputManager manager;
+
     // Start is called before the first frame update
     void Start()
     {
+        // Get the PlayerInputManager component on this object
         manager = GetComponent<PlayerInputManager>();
+
+        // Set the initial player prefab to the first fighter in the list
         manager.playerPrefab = fighters[index];
     }
 
+    // Method that switches to the next fighter in the list
     public void SwitchNextSpawnCharacter(PlayerInput input)
     {
+        // Increment the index to select the next fighter in the list
         index++;
+
+        // If the index is greater than or equal to the number of fighters in the list, wrap around to the first fighter
+        if (index >= fighters.Count)
+        {
+            index = 0;
+        }
+
+        // Set the player prefab to the selected fighter
         manager.playerPrefab = fighters[index];
     }
 }
