a39f9c888877650e625d3b1b5c4b7b64663d1b98
pikaparv <pikaparv>
2023-04-17 14:15:28 +0300

    charge muokkailua

--- Assets/Script/PlayerAbilities/Kipina/DeviceOpenDoor.cs
+++ Assets/Script/PlayerAbilities/Kipina/DeviceOpenDoor.cs
@@ -1,18 +1,11 @@
 using System.Collections;
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-// ©GameGurus - Heikkinen R., Hopeasaari J., Kantola J., Kettunen J., Kommio R, PC, Parviainen P., Rautiainen J.
-// By: Parviainen P
-//  Edited: Kettunen J
-// The device becomes active and moves child-object attached to the device x amount of times up and down.
-// Nulls all changes and variables, then stops.
-
-public class DeviceCharged : MonoBehaviour
+public class DeviceOpenDoor : MonoBehaviour
 {
     //Atributes visible in unity
-    [Tooltip("How many times activated device moves child-object")] [SerializeField] private int times = 4;
+    //[Tooltip("How many times activated device moves child-object")] [SerializeField] private int times = 4;
     [Tooltip("How high the target-position is from the start-position")] [SerializeField] private float height = 3f;
     [Tooltip("How much child-object is moved per frame")] [SerializeField] private float force = 0.02f;
 
@@ -22,11 +15,12 @@ public class DeviceCharged : MonoBehaviour
 
     //Private attributes
     private GameObject moveable;    // The object that device moves when activated (= child-object)
+    private GameObject doorTrigger;
     private Rigidbody rb;           // The rigidbidy of the moveable-object
     private Vector3 targetP;        // Target position where object is moved
     private Vector3 startP;         // Start position for the object
     private int trips = 0;          // How many trips the object has made between positions so far
-    
+
     /// <summary>
     /// Sets audiosource 
     /// Disables this script
@@ -47,13 +41,14 @@ public class DeviceCharged : MonoBehaviour
     {
         moveable = transform.GetChild(0).gameObject;
         if (moveable != null)
-        {            
+        {
             rb = moveable.GetComponent<Rigidbody>();
             rb.constraints &= ~RigidbodyConstraints.FreezePositionY;
             rb.useGravity = false;
             startP = moveable.transform.position;
             targetP = moveable.transform.position;
             targetP.y = (moveable.transform.position.y + height);
+            doorTrigger = moveable.transform.GetChild(0).gameObject;
             audioSource.clip = chargedDeviceSound;
             audioSource.loop = true;
             audioSource.Play();
@@ -74,8 +69,18 @@ public class DeviceCharged : MonoBehaviour
         moveable = null;
         rb = null;
         audioSource.Stop();
-
         transform.GetComponent<DeviceCharged>().enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (moveable != null)
+        {
+            if(Vector3.Distance(rb.transform.position, targetP) < 0.001f)
+            {
+                StopScript();
+            }
+        }
     }
 
     /// <summary>
@@ -88,18 +93,14 @@ public class DeviceCharged : MonoBehaviour
         if (moveable != null)
         {
             rb.transform.position = Vector3.MoveTowards(rb.transform.position, targetP, force);
-            if (true && (Vector3.Distance(rb.transform.position, targetP) < 0.001f)) // Hups onkohan tässä jotain kesken koska mitä toi true tossa on???
+            /*if (true && (Vector3.Distance(rb.transform.position, targetP) < 0.001f)) // Hups onkohan tässä jotain kesken koska mitä toi true tossa on???
             {
                 float temp = targetP.y;
                 targetP.y = startP.y;
                 startP.y = temp;
                 trips++;
-            }
-            if (trips == times)
-            {
-                StopScript();
-            }
+            }*/
         }
-        
+
     }
 }
