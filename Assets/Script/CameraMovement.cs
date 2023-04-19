145f289df9a9bd989434fb314fa7c6ef7cd7c833
kettjbtz <janne.b.kettunen@student.jyu.fi>
2023-04-19 23:01:56 +0300

    kameralle rotate skripti ja kipinä hiekkalaatikolle

--- Assets/Script/CameraMovement.cs
+++ Assets/Script/CameraMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 // ©GameGurus - Heikkinen R., Hopeasaari J., Kantola J., Kettunen J., Kommio R, PC, Parviainen P., Rautiainen J.
 // Creator: Kettunen. J
@@ -9,11 +10,14 @@ public class CameraMovement : MonoBehaviour
 {
     [Tooltip("How far the camera is away from players")] [SerializeField] private float zoomFactor = 15f;
     [Tooltip("The height of the camera from groundlevel")] [SerializeField] private float cameraHeight = 13f;
-    [Tooltip("Follow speed of the camera")] [SerializeField] private float follofSpeed = 2f;
+    [Tooltip("Follow speed of the camera")] [SerializeField] private float followSpeed = 2f;
 
     private GameObject[] players;
     private Vector3[] playerPositions = new Vector3[2];
-
+    private Vector3 midpoint;
+    private float rotateSpeed;
+    private float amountRotated = 0;
+    private float rotateDegrees;
     /// <summary>
     /// Moves the camera according to the center point of the two players
     /// </summary>
@@ -22,13 +26,13 @@ public class CameraMovement : MonoBehaviour
         if (players.Length == 2)
         {
             // Calculating the midpoint of the players
-            Vector3 midpoint = (players[0].transform.position + players[1].transform.position) / 2f;
+            midpoint = (players[0].transform.position + players[1].transform.position) / 2f;
             midpoint.y = playerPositions[0].y + playerPositions[1].y / 2;
 
             // Moving the camera
             Vector3 cameraDestination = midpoint - transform.forward * zoomFactor;
             cameraDestination.y = cameraHeight + midpoint.y;
-            transform.position = Vector3.Slerp(transform.position, cameraDestination, follofSpeed * Time.deltaTime);
+            transform.position = Vector3.Slerp(transform.position, cameraDestination, followSpeed * Time.deltaTime);
         }
         if (players.Length == 1) //Added for testing with one player
         {
@@ -38,12 +42,28 @@ public class CameraMovement : MonoBehaviour
             // Moving the camera
             Vector3 cameraDestination = cameraTarget - transform.forward * zoomFactor;
             cameraDestination.y = cameraHeight;
-            transform.position = Vector3.Slerp(transform.position, cameraDestination, follofSpeed * Time.deltaTime);
+            transform.position = Vector3.Slerp(transform.position, cameraDestination, followSpeed * Time.deltaTime);
         }
     }
 
+    public void RotateCamera(float rotDeg, float rotateSpeed)
+    {
+        this.rotateSpeed = rotateSpeed;
+        rotateDegrees = rotDeg;
+    }
     void Update()
     {
+        float rotate = 0;
+        if (amountRotated < rotateDegrees)
+        {
+            rotate = rotateSpeed * Time.deltaTime;
+            amountRotated += rotate;
+        }
+        if (rotate != 0)
+        {
+            transform.RotateAround(midpoint, Vector3.up, rotate);
+        }
+
         //Finding the players from the scene
         players = GameObject.FindGameObjectsWithTag("player");
         for (int i = 0; i < players.Length; i++)
