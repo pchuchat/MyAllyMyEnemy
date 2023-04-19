830b908a5bcedd33662004c30bd6542235d8d850
kettjbtz <janne.b.kettunen@student.jyu.fi>
2023-04-19 19:51:35 +0300

    kameran y liike ja followspeed vaihtoehto lisätty

--- Assets/Script/CameraMovement.cs
+++ Assets/Script/CameraMovement.cs
@@ -9,24 +9,26 @@ public class CameraMovement : MonoBehaviour
 {
     [Tooltip("How far the camera is away from players")] [SerializeField] private float zoomFactor = 15f;
     [Tooltip("The height of the camera from groundlevel")] [SerializeField] private float cameraHeight = 13f;
+    [Tooltip("Follow speed of the camera")] [SerializeField] private float follofSpeed = 2f;
 
     private GameObject[] players;
+    private Vector3[] playerPositions = new Vector3[2];
 
     /// <summary>
     /// Moves the camera according to the center point of the two players
     /// </summary>
     public void MoveCamera()
     {
-        float followTimeDelta = 0.8f;
         if (players.Length == 2)
         {
             // Calculating the midpoint of the players
             Vector3 midpoint = (players[0].transform.position + players[1].transform.position) / 2f;
+            midpoint.y = playerPositions[0].y + playerPositions[1].y / 2;
 
             // Moving the camera
             Vector3 cameraDestination = midpoint - transform.forward * zoomFactor;
-            cameraDestination.y = cameraHeight;
-            transform.position = Vector3.Slerp(transform.position, cameraDestination, followTimeDelta);
+            cameraDestination.y = cameraHeight + midpoint.y;
+            transform.position = Vector3.Slerp(transform.position, cameraDestination, follofSpeed * Time.deltaTime);
         }
         if (players.Length == 1) //Added for testing with one player
         {
@@ -36,7 +38,7 @@ public class CameraMovement : MonoBehaviour
             // Moving the camera
             Vector3 cameraDestination = cameraTarget - transform.forward * zoomFactor;
             cameraDestination.y = cameraHeight;
-            transform.position = Vector3.Slerp(transform.position, cameraDestination, followTimeDelta);
+            transform.position = Vector3.Slerp(transform.position, cameraDestination, follofSpeed * Time.deltaTime);
         }
     }
 
@@ -44,8 +46,13 @@ public class CameraMovement : MonoBehaviour
     {
         //Finding the players from the scene
         players = GameObject.FindGameObjectsWithTag("player");
-
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i].GetComponent<CharacterController>().isGrounded)
+            {
+                playerPositions[i] = players[i].transform.position;
+            }
+        }
         MoveCamera();
     }
-    
 }
