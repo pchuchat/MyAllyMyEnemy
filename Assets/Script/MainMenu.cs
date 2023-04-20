06975930cdf33e1ed1da8c6d159cf3f9a35fa2e8
Phatchanon Chuchat <pchuchat@student.jyu.fi>
2023-04-20 12:14:40 +0300

    pause menu jäi kesken

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject Point;
+    public Canvas menuCanvas;
+
+    private int SelectedButton = 1;
+    [SerializeField]
+    private int NumberOfButtons;
+
+    public Transform ButtonPosition1;
+    public Transform ButtonPosition2;
+    public Transform ButtonPosition3;
+    public Transform ButtonPosition4;
+
+
+    private void OnPlay()
+    {
+        if (SelectedButton == 1)
+        {
+            // When the button with the pointer is clicked, this piece of script is activated
+
+            StartCoroutine(LoadScenesAndHideMenu());
+
+        }
+        else if (SelectedButton == 2)
+        {
+            // When the button with the pointer is clicked, this piece of script is activated
+            Application.Quit();
+        }
+    }
+    private IEnumerator LoadScenesAndHideMenu()
+    {
+        // Unload the current scene (main menu) and load the desired scenes additively
+        SceneManager.UnloadSceneAsync(0);
+        yield return SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive); // Load MainScene
+        yield return SceneManager.LoadSceneAsync(2, LoadSceneMode.Additive); // Load the level
+
+        menuCanvas.gameObject.SetActive(false); // Hide the menu
+    }
+    private void OnButtonUp()
+    {
+        // Checks if the pointer needs to move down or up, in this case the poiter moves up one button
+        if (SelectedButton > 1)
+        {
+            SelectedButton -= 1;
+        }
+        MoveThePointer();
+        return;
+    }
+    private void OnButtonDown()
+    {
+        // Checks if the pointer needs to move down or up, in this case the poiter moves down one button
+        if (SelectedButton < NumberOfButtons)
+        {
+            SelectedButton += 1;
+        }
+        MoveThePointer();
+        return;
+    }
+    private void MoveThePointer()
+    {
+        // Moves the pointer
+        if (SelectedButton == 1)
+        {
+            Point.transform.position = ButtonPosition1.position;
+        }
+        else if (SelectedButton == 2)
+        {
+            Point.transform.position = ButtonPosition2.position;
+        }
+        else if (SelectedButton == 3)
+        {
+            Point.transform.position = ButtonPosition3.position;
+        }
+        else if (SelectedButton == 4)
+        {
+            Point.transform.position = ButtonPosition4.position;
+        }
+    }
+
+}
