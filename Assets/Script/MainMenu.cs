b7afc15341d6b1b482735ffe2a3a558898363296
Joonas Hopeasaari <joonas.hopeasaari@gmail.com>
2023-05-22 19:06:58 +0300

    fadet main menuun

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -1,15 +1,44 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator animator;
+    public PlayerInput playerInput;
+
+    private int action;
+
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+    }
+
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        playerInput.DeactivateInput();
+        action = 1;
+        animator.SetTrigger("FadeOut");
     }
 
     public void QuitGame()
     {
-        Application.Quit();
+        playerInput.DeactivateInput();
+        action = 2;
+        animator.SetTrigger("FadeOut");
+    }
+
+    // Start the game -> action = 1
+    // Quit the game --> action = 2
+    public void OnFadeComplete()
+    {
+        if (action == 1)
+        {
+            SceneManager.LoadScene(1);
+        }
+        if (action == 2)
+        {
+            Application.Quit();
+        }
     }
 }
